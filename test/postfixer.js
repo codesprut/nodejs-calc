6d77e2f18ec24d842e313cf2acea83013250959c
CodeSprut <Alex-p.s@yandex.ru>
2019-10-07 17:18:33 +0500

    cleanup and prettify postfixer tests

--- test/postfixer.js
+++ test/postfixer.js
@@ -2,35 +2,31 @@ const Postfixer = require('../postfixer');
 const assert = require("assert");
 
 describe("Postfixer tests", function() {
-	it("2 + ( 4 - 99 ) / 2 to 2 4 + 99 -", function() {
+	it("Simple expression with brackets", function() {
 		assert.equal(Postfixer.convert('2 + ( 4 - 99 ) / 2'), '2 4 99 - 2 / +');
 	});
 
-	it("4 + ( - ( - ( -4 ) ) ) / 2 to 4 4 - - - 2 / +", function() {
+	it("Many signs in a row", function() {
 		assert.equal(Postfixer.convert('4 + ( - ( - ( -4 ) ) ) / 2'), '4 -4 - - 2 / +');
 	});
 
-	it("sqrt ( 5 ) to . 5 sqrt", function() {
-		assert.equal(Postfixer.convert('sqrt ( 5 )'), '. 5 sqrt');
-	});
-
-	it("sqrt ( 4 ) * 5 = . 4 sqrt 5 *", function() {
-		assert.equal(Postfixer.convert('sqrt ( 4 ) * 5'), '. 4 sqrt 5 *');
-	});
-
-	it("-4 - 4 to -4 4 -", function() {
+	it("Negative to negative", function() {
 		assert.equal(Postfixer.convert('-4 - 4'), '-4 4 -');
 	});
 
-	it("pow ( 2 , 3 ) to . 2 3 pow", function() {
+	it("Math function", function() {
+		assert.equal(Postfixer.convert('sqrt ( 5 )'), '. 5 sqrt');
+	});
+
+	it("Math function with another operand", function() {
+		assert.equal(Postfixer.convert('sqrt ( 4 ) * 5'), '. 4 sqrt 5 *');
+	});
+
+	it("Math function with multiple params", function() {
 		assert.equal(Postfixer.convert('pow ( 2 , 3 )'), '. 2 3 pow');
 	});
 
-	it("-1 + pow ( 2 , 3 ) to -1 . 2 3 pow +", function() {
-		assert.equal(Postfixer.convert('-1 + pow ( 2 , 3 )'), '-1 . 2 3 pow +');
-	});
-
-	it("max ( 10 , 20 + 5 , 30 - 40 , min ( 40 , 100 ) )", function() {
+	it("Nested functions and operations in commas", function() {
 		assert.equal(Postfixer.convert('max ( 10 , 20 + 5 , 30 - 40 , min ( 40 , 100 ) )'), '. 10 20 5 + 30 40 - . 40 100 min max');
 	});
 });
